4d6cab3932c0d4e2fc4a3e840daa20477ecd6961
gonzaponte <gonzaponte@gmail.com>
2015-08-08 22:35:03 +0200

    Particle class implemented

---
+++ EI/Particle.cc
@@ -0,0 +1,391 @@
+///////////////////////////////////////////////////////////////////////////////
+////
+////  @Author   Gonzalo Martínez Lema
+////  @Date     08/08/2015
+////  @Mofidied 08/08/2015
+////
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "Particle.hh"
+#include "Units.hh"
+#include "Track.hh"
+
+#include <iostream>
+
+ClassImp(nuEI::Particle);
+
+namespace nuEI
+{
+  Particle::Particle() :
+  _particleID(0), _PDGcode(0), _name("Unknown"),
+  _mass(0), _charge(0), _lifetime(0), _track_length(0.), _primary(true),
+  _initial_vertex(), _decay_vertex(), _initial_momentum(), _decay_momentum(),
+  _initial_volume("Unknown"), _decay_volume("Unknown"), _creator_process("Unknown"),
+  _mother(0), _tracks(0), _daughters(0),
+  _PDG_manager()
+  {  }
+
+  Particle::Particle(int pdg_code) :
+  _particleID(0),
+  _track_length(0.), _primary(true),
+  _initial_vertex(), _decay_vertex(), _initial_momentum(), _decay_momentum(),
+  _initial_volume("Unknown"), _decay_volume("Unknown"), _creator_process("Unknown"),
+  _mother(0), _tracks(0), _daughters(0),
+  _PDG_manager()
+  {
+    SetPDGcode(pdg_code);
+    _decay_momentum.SetXYZT(0.,0.,0.,_mass);
+  }
+
+  Particle::Particle(std::string name) :
+  _particleID(0),
+  _track_length(0.), _primary(true),
+  _initial_vertex(), _decay_vertex(), _initial_momentum(), _decay_momentum(),
+  _initial_volume("Unknown"), _decay_volume("Unknown"), _creator_process("Unknown"),
+  _mother(0), _tracks(0), _daughters(0),
+  _PDG_manager()
+  {
+    int code = _PDG_manager.GetParticle(name)->PdgCode();
+    *this = Particle(code);
+  }
+
+  Particle::~Particle()
+  {
+    _daughters.Delete();
+  }
+
+  void Particle::SetPDGcode( int code )
+  {
+    _PDGcode = code;
+    AddProperties();
+  }
+
+  void Particle::AddProperties()
+  {
+    TParticlePDG* PDGparticle = (TParticlePDG*) _PDG_manager.GetParticle(_PDGcode);
+    _name     = PDGparticle->GetName();
+    _mass     = PDGparticle->Mass();
+    _charge   = PDGparticle->Charge();
+    _lifetime = PDGparticle->Lifetime();
+  }
+
+  const Particle* Particle::GetMother() const
+  {
+    if (!_mother.GetObject())
+      std::cerr << "======================================= "
+                << "Error in nuEI::Particle::GetMother():   "
+    		        << "The particle is primary, no mother set. "
+                << "======================================= " << std::endl;
+
+    return dynamic_cast<Particle*> (_mother.GetObject());
+  }
+  //
+  // Particle* Particle::GetMother()
+  // {
+  //   if (!_mother.GetObject()) {
+  //     std::cerr << "[ERROR: nuEI::Particle::GetMother()]:"
+  //   		<< " the particle is primary, it has no mother!" << std::endl;
+  //   }
+  //   return dynamic_cast<Particle*> (_mother.GetObject());
+  // }
+
+  // Track* Particle::GetTrack(const std::string& det)
+  // {
+  //   for (int i=0; i< _tracks.GetLast()+1; ++i) {
+  //     Track* tr = (Track*)_tracks.At(i);
+  //     if (tr->GetDetectorName() == det) {
+	//  return tr;
+  //     }
+  //   }
+  //   // std::cerr << "[ERROR: nuEI::Particle::GetTrack()]:"
+  //   // 	      << " the particle doesn't have hits from detector "
+  //   // 	      << det << std::endl;
+  //   return 0;
+  //
+  // }
+
+  // void Particle::SetName(int code)
+  // {
+  //   if (code == 22) {
+  //     _name = "gamma";
+  //     SetParameters(0,0,-1);
+  //   } else if (code == -11) {
+  //     _name = "e+";
+  //     SetParameters(5.10998902E-04*GeV,1,-1);
+  //   } else if (code == 11) {
+  //     _name = "e-";
+  //     SetParameters(5.10998902E-04*GeV,-1,-1);
+  //   } else if (code == -13) {
+  //     _name = "mu+";
+  //     SetParameters(1.05658357E-01*GeV,1,2.197e-6*second);
+  //   } else if (code == 13) {
+  //     _name = "mu-";
+  //     SetParameters(1.05658357E-01*GeV,1,2.197e-6*second);
+  //   } else if (code == -15) {
+  //     _name = "tau+";
+  //     SetParameters(1.77699*GeV,1,291e-15*second);
+  //   } else if (code == 15) {
+  //     _name = "tau-";
+  //     SetParameters(1.77699*GeV,-1,291e-15*second);
+  //   } else if (code == 12) {
+  //     _name = "nu_e";
+  //     SetParameters(0,0,-1);
+  //   } else if (code == -12) {
+  //     _name = "anti_nu_e";
+  //     SetParameters(0,0,-1);
+  //   } else if (code == 14) {
+  //     _name = "nu_mu";
+  //     SetParameters(0,0,-1);
+  //   } else if (code == -14) {
+  //     _name = "anti_nu_mu";
+  //     SetParameters(0,0,-1);
+  //   }  else if (code == 16) {
+  //     _name = "nu_tau";
+  //     SetParameters(0,0,-1);
+  //   } else if (code == -16) {
+  //     _name = "anti_nu_tau";
+  //     SetParameters(0,0,-1);
+  //   } else if (code == 211) {
+  //     _name = "pi+";
+  //     SetParameters(1.3957018E-01*GeV,1,2.603e-8*second);
+  //   } else if (code == -211) {
+  //     _name = "pi-";
+  //     SetParameters(1.3957018E-01*GeV,-1,2.603e-8*second);
+  //   } else if (code == 111) {
+  //     _name = "pi0";
+  //     SetParameters(1.349766E-01*GeV,0,8.4e-17*second);
+  //   } else if (code == 321) {
+  //     _name = "kaon+";
+  //     SetParameters(4.93677E-01*GeV,1,1.2386e-8*second);
+  //   } else if (code == -321) {
+  //     _name = "kaon-";
+  //     SetParameters(4.93677E-01*GeV,-1,1.2386e-8*second);
+  //   } else if (code == 130) {
+  //     _name = "kaon0L";
+  //     SetParameters(4.97672E-01*GeV,0,5.17e-8*second);
+  //   } else if (code == 311) {
+  //     _name = "kaon0";
+  //     SetParameters(4.97672E-01*GeV,0,0.89e-10*second);
+  //   } else if (code == 2212) {
+  //     _name = "proton";
+  //     SetParameters(9.3827200E-01*GeV,1,-1);
+  //   } else if (code == -2212) {
+  //     _name = "anti_proton";
+  //     SetParameters(9.3827200E-01*GeV,-1,-1);
+  //   } else if (code == 2112) {
+  //     _name = "neutron";
+  //     SetParameters(9.3956533E-01*GeV,0,-1);
+  //   } else if (code == -2112) {
+  //     _name = "anti_neutron";
+  //     SetParameters(9.3956533E-01*GeV,0,-1);
+  //   } else if (code == 1000020040) {
+  //     _name = "alpha";
+  //     SetParameters(3.727*GeV,0,-1);
+  //   }
+  //
+  // }
+
+
+  void Particle::Info(ostream& s) const
+  {
+    s << std::endl
+      << "Particle name:      " << _name       << std::endl
+      << "Particle ID:        " << _particleID << std::endl
+      << "PDG code:           " << _PDGcode    << std::endl
+      << "Mass (MeV):         " << _mass       << std::endl
+      << "Charge:             " << _charge     << std::endl;
+
+    s << std::endl
+      << "------------------------------------" << std::endl
+      << " Production:"                         << std::endl
+      << "------------------------------------" << std::endl;
+
+    s << "Volume:               " << _initial_volume << std::endl;
+      if (_mother.GetObject())
+        s << "Mother:               " << _mother->GetName() << " with ID " << _mother.GetID() << std::endl
+          << "Creator process:      " << _creator_process << std::endl;
+      else
+        s << "Particle is primary   " << std::endl;
+
+    s << "Vertex (mm):          " << _initial_vertex.X()/mm << " " << _initial_vertex.Y()/mm << " " << _initial_vertex.Z()/mm << std::endl
+      << "3-momentum (MeV):     " << _initial_momentum.X()/MeV << " " << _initial_momentum.Y()/MeV << " " << _initial_momentum.Z()/MeV << std::endl
+      << "Total momentum (MeV): " << InitialMomentum()/MeV << std::endl
+      << "Energy (MeV):         " << InitialEnergy()/MeV << std::endl;
+
+    s << std::endl
+      << "------------------------------------" << std::endl
+      << " Decay:"                         << std::endl
+      << "------------------------------------" << std::endl;
+
+    s << "Volume:               " << _decay_volume << std::endl
+      << "Vertex (mm):          " << _decay_vertex.X()/mm << " " << _decay_vertex.Y()/mm << " " << _decay_vertex.Z()/mm << std::endl
+      << "3-momentum (MeV):     " << _decay_momentum.X()/MeV << " " << _decay_momentum.Y()/MeV << " " << _decay_momentum.Z()/MeV << std::endl
+      << "Total momentum (MeV): " << DecayMomentum()/MeV << std::endl
+      << "Energy (MeV):         " << DecayEnergy()/MeV << std::endl;
+
+    s << std::endl
+      << "------------------------------------" << std::endl
+      << " Secondary particles:               " << std::endl
+      << "------------------------------------" << std::endl;
+
+    for (int i=0; i<=_daughters.GetLast(); ++i)
+    {
+      Particle* p = (Particle*) _daughters.At(i);
+      s << p->Name() << " with ID "	<< p->GetID << std::endl;
+      //   << "Mass (MeV):       " << p->GetMass()/MeV << std::endl
+	    //   << "Charge:           " << p->GetCharge()	<< std::endl
+      //   << "3-momentum (MeV): " << p->GetInitialMomentum().X()/MeV << " " << p->GetInitialMomentum().Y()/MeV << " " << p->GetInitialMomentum().Z()/MeV << std::endl
+      //   << "Total momentum (MeV): " << p->Momentum()/MeV << std::endl;
+      // s << "particle energy (MeV)= " << p->Energy()/MeV << std::endl;
+    }
+
+    s << std::endl
+      << "------------------------------------" << std::endl
+      << " True hits:                         " << std::endl
+      << "------------------------------------" << std::endl;
+
+    for (int i=0; i<=_tracks.GetLast(); ++i)
+    {
+      Track* t = (Track*)_tracks.At(i);
+      s << *t << std::endl << std::endl;
+    }
+
+    s << "Total track length (mm) = " << _track_length()/mm << std::endl;
+  }
+  //
+  // void Particle::SetParticleName(const std::string& name)
+  // {
+  //   _name = name;
+  //
+  //   if (name == "gamma"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode = 22;
+  //   }
+  //   else if (name == "e+"){
+  //     SetParameters(5.10998902E-04*GeV,1,-1);
+  //     _PDGcode  = -11;
+  //   }
+  //   else if (name == "e-"){
+  //     SetParameters(5.10998902E-04*GeV,-1,-1);
+  //     _PDGcode  = 11;
+  //   }
+  //   else if (name == "mu-"){
+  //     SetParameters(1.05658357E-01*GeV,-1,2.197e-6*second);
+  //     _PDGcode  = 13;
+  //   }
+  //   else if (name == "mu+"){
+  //     SetParameters(1.05658357E-01*GeV,1,2.197e-6*second);
+  //     _PDGcode  = -13;
+  //   }
+  //   else if (name == "tau+"){
+  //     SetParameters(1.77699*GeV,1,291e-15*second);
+  //     _PDGcode  = -15;
+  //   }
+  //   else if (name == "tau-"){
+  //     SetParameters(1.77699*GeV,-1,2.291e-15*second);
+  //     _PDGcode  = 15;
+  //   }
+  //   else if (name == "nu_e"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode  = 12;
+  //   }
+  //   else if (name == "anti_nu_e"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode  = -12;
+  //   }
+  //   else if (name == "nu_mu"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode  = 14;
+  //   }
+  //   else if (name == "anti_nu_mu"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode = -14;
+  //   }
+  //   else if (name == "nu_tau"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode  = 16;
+  //   }
+  //   else if (name == "anti_nu_tau"){
+  //     SetParameters(0,0,-1);
+  //     _PDGcode  = -16;
+  //   }
+  //   else if (name == "pi+"){
+  //     SetParameters(1.3957018E-01*GeV,1,2.603e-8*second);
+  //     _PDGcode = 211;
+  //   }
+  //   else if (name == "pi-"){
+  //     SetParameters(1.3957018E-01*GeV,-1,2.603e-8*second);
+  //     _PDGcode  = -211;
+  //   }
+  //   else if (name == "pi0"){
+  //     SetParameters(1.349766E-01*GeV,0,8.4e-17*second);
+  //     _PDGcode  = 111;
+  //   }
+  //   else if (name == "kaon+"){
+  //     SetParameters(4.93677E-01*GeV,1,1.2386e-8*second);
+  //     _PDGcode  = 321;
+  //   }
+  //   else if (name == "kaon-"){
+  //     SetParameters(4.93677E-01*GeV,-1,1.2386e-8*second);
+  //     _PDGcode  = -321;
+  //   }
+  //   else if (name == "kaon0L"){
+  //     SetParameters(4.97672E-01*GeV,0,5.17e-8*second);
+  //     _PDGcode  = 130;
+  //   }
+  //   else if (name == "kaon0"){
+  //     SetParameters(4.97672E-01*GeV,0,0.89e-10*second);
+  //     _PDGcode  = 310;
+  //   }
+  //   else if (name == "proton"){
+  //     SetParameters(9.3827200E-01*GeV,1,-1);
+  //     _PDGcode  = 2212;
+  //   }
+  //   else if (name == "anti_proton"){
+  //     SetParameters(9.3827200E-01*GeV,-1,-1);
+  //     _PDGcode  = -2212;
+  //   }
+  //   else if (name == "neutron"){
+  //     SetParameters(9.3956533E-01*GeV,0,-1);
+  //     _PDGcode  = 2112;
+  //   }
+  //   else if (name == "anti_neutron"){
+  //     SetParameters(9.3956533E-01*GeV,0,-1);
+  //     _PDGcode = -2112;
+  //   }
+  //   else if (name == "deuteron"){
+  //     SetParameters(1.875613*GeV,0,-1);
+  //     _PDGcode  = 0;
+  //   }
+  //   else if (name == "tritium"){
+  //     SetParameters(2.80925*GeV,0,-1);
+  //     _PDGcode  = 0;
+  //   }
+  //   else if (name == "alpha"){
+  //     SetParameters(3.727*GeV,0,-1);
+  //     _PDGcode  = 0;
+  //   }
+  //   else if (name == "unknown"){ // assume pion mass
+  //     SetParameters(1.3957018E-01*GeV,-1,2.603e-8*second);
+  //     _PDGcode  = 0;
+  //   }
+  //   else if (name == "void"){
+  //     SetParameters(0,0,0);
+  //     _PDGcode  = 0;
+  //   }
+  //   else {
+  //     SetParameters(0,0,-1);
+  //     _PDGcode=0;
+  //   }
+  //
+  // }
+
+
+
+} // namespace nuEI
+
+ostream& operator << (ostream& s, const nuEI::Particle& p) {
+  p.Info(s);
+  return s;
+}
---
+++ EI/Particle.hh
@@ -0,0 +1,184 @@
+///////////////////////////////////////////////////////////////////////////////
+////
+////  Class with the description of a particle.
+////
+////  @Author   Gonzalo Martínez Lema
+////  @Date     07/08/2015
+////  @Mofidied 07/08/2015
+////
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __nuEI_PARTICLE__
+#define __nuEI_PARTICLE__
+
+#include <string>
+#include <map>
+
+#include <TObject.h>
+#include <TLorentzVector.h>
+#include <TRef.h>
+#include <TRefArray.h>
+
+namespace nuEI { class Track; }
+
+namespace nuEI
+{
+  class Particle : public TObject
+  {
+
+  public:
+    // Empty constructor
+    Particle();
+    // Named constructor
+    Particle(std::string name);
+    // Code constructor
+    Particle(int pdg);
+    /// Destructor
+    ~Particle();
+
+  private:
+    int _particleID;                  // Geant4 ID
+    int _PDGcode;                     // PDG code of the particle
+    std::string _name;                // name of the particle
+
+    double _mass;                     // mass. It's set through the PDG
+    double _charge;                   // charge. It's set through the PDG
+    double _lifetime;                 // life time. It's set through the PDG
+    double _track_length;             // length of the associated track
+    bool   _primary;                  // true if the particle is primary (shot by Geant)
+
+    TLorentzVector _initial_vertex;   // point and time of generation
+    TLorentzVector _decay_vertex;     // point and time of death
+    TLorentzVector _initial_momentum; // initial momentum and energy
+    TLorentzVector _decay_momentum;   // final momentum and energy
+
+    std::string _initial_volume;       // name of the volume where the particle was created
+    std::string _decay_volume;        // name of the volume where the particle decays
+    std::string _creator_process;     // name of the process by which the particle was created
+
+    TRef      _mother;                // reference to the mother particle
+    TRefArray _tracks;                // reference to the true hits made by the particle
+    TRefArray _daughters;             // array of references to the secondary particles comming from this one
+
+    TDatabasePDG _PDG_manager;        // Interface with PDG database
+
+    AddProperties();                  // Set properties from PDG database
+  public:
+    void SetParticleID(int ID);
+    int  GetParticleID() const;
+    void SetPDGcode(int code);
+    int  GetPDGcode() const;
+    void SetName(const std::string& name);
+    void SetName(int code);
+    const std::string& GetName() const;
+
+    double GetMass() const;
+    double GetCharge() const;
+    double InitialEnergy() const;
+    double InitialMomentum() const;
+    double DecayEnergy() const;
+    double DecayMomentum() const;
+    void   SetTrackLength(double L);
+    double GetTrackLength() const;
+    void   SetPrimary(bool primary);
+    bool   IsPrimary() const;
+
+    void SetInitialVertex(double x, double y, double z, double t);
+    void SetInitialVertex(TLorentzVector V);
+    TLorentzVector GetInitialVertex() const;
+    void GetInitialVertex(double xyzt[]) const;
+
+    void SetDecayVertex(double x, double y, double z, double t);
+    void SetDecayVertex(TLorentzVector V);
+    TLorentzVector GetDecayVertex() const;
+    void GetDecayVertex(double xyzt[]) const;
+
+    void SetInitialMomentum(double x, double y, double z, double E);
+    void SetInitialMomentum(TLorentzVector V);
+    TLorentzVector GetInitialMomentum() const;
+    void GetInitialMomentum( double xyze[]) const;
+
+    void SetDecayMomentum(double x, double y, double z, double E);
+    void SetDecayMomentum(TLorentzVector V);
+    TLorentzVector GetDecayMomentum() const;
+    void GetDecayMomentum( double xyze[] ) const;
+
+    void SetInitialVolume(const std::string& volume);
+    const std::string& GetInitialVolume() const;
+    void SetDecayVolume(const std::string& volume);
+    const std::string& GetDecayVolume() const;
+    void SetCreatorProcess(const std::string& process);
+    const std::string& GetCreatorProcess() const;
+
+    void SetMother(Particle* mother);
+    const Particle* GetMother() const;
+    void AddTrack(Track* track);
+    const TRefArray GetTracks() const;
+    Track* GetTrack(const std::string& det);
+    void AddDaughter(Particle* daughter);
+    const TRefArray GetDaughters() const;
+
+    void Info(ostream& s) const;
+
+    ClassDef(Particle,4);
+
+  }; // class Particle
+
+  // INLINE functions
+  inline void Particle::SetParticleID(int ID) {_particleID = ID;}
+  inline int  Particle::GetParticleID() const {return _particleID;}
+  inline void Particle::SetPDGcode(int code) {_PDGcode = code;AddProperties()}
+  inline int  Particle::GetPDGcode() const {return _PDGcode;}
+  inline void Particle::SetName( const std::string& name ) {_name = name;}
+  inline const std::string& Particle::GetName() const {return _name;}
+
+  inline double Particle::GetMass()         const {return _mass;}
+  inline double Particle::GetCharge()       const {return _charge;}
+  inline double Particle::InitialEnergy()   const {return _initial_momentum.Energy();}
+  inline double Particle::InitialMomentum() const {return _initial_momentum.P();}
+  inline double Particle::DecayEnergy()     const {return _decay_momentum.Energy();}
+  inline double Particle::DecayMomentum()   const {return _decay_momentum.P();}
+  inline void   Particle::SetTrackLength(double L){ _track_length = L;}
+  inline double Particle::GetTrackLength()  const {return _track_length;}
+  inline void   Particle::SetPrimary(bool primary){_primary = primary;}
+  inline bool   Particle::IsPrimary()        const{return _primary;}
+
+  inline void Particle::SetInitialVertex(double x, double y, double z, double t){ _initial_vertex.SetXYZT(x,y,z,t);}
+  inline void Particle::SetInitialVertex(TLorentzVector V){ _initial_vertex = V;}
+  inline TLorentzVector Particle::GetInitialVertex() const{ return _initial_vertex;}
+  inline void Particle::GetInitialVertex(double xyzt[]) const{ _initial_vertex.GetXYZT( xyzt ); return;}
+
+  inline void Particle::SetDecayVertex(double x, double y, double z, double t){ _decay_vertex.SetXYZT(x,y,z,t);}
+  inline void Particle::SetDecayVertex(TLorentzVector V){ _decay_vertex = V;}
+  inline TLorentzVector Particle::GetDecayVertex() const{ return _decay_vertex;}
+  inline void Particle::GetDecayVertex(double xyzt[]) const{ _decay_vertex.GetXYZT( xyzt ); return;}
+
+  inline void Particle::SetInitialMomentum(double x, double y, double z, double E){ _initial_momentum.SetXYZT(x,y,z,E);}
+  inline void Particle::SetInitialMomentum(TLorentzVector V){ _initial_momentum = V;}
+  inline TLorentzVector Particle::GetInitialMomentum() const{ return _initial_momentum;}
+  inline void Particle::GetInitialMomentum( double xyze[]) const{ _initial_momentum.GetXYZT( xyze ); return;}
+
+  inline void Particle::SetDecayMomentum(double x, double y, double z, double E){ _decay_momentum.SetXYZT(x,y,z,E);}
+  inline void Particle::SetDecayMomentum(TLorentzVector V){ _decay_momentum = V;}
+  inline TLorentzVector Particle::GetDecayMomentum() const{ return _decay_momentum;}
+  inline void Particle::GetDecayMomentum( double xyze[] ) const{ _decay_momentum.GetXYZT( xyze ); return;}
+
+  inline void  Particle::SetInitialVolume(const std::string& volume){ _initial_volume = volume;}
+  inline const std::string& Particle::GetInitialVolume() const{ return _initial_volume;}
+  inline void  Particle::SetDecayVolume(const std::string& volume){ _decay_volume = volume;}
+  inline const std::string& Particle::GetDecayVolume() const{ return _decay_volume;}
+  inline void  Particle::SetCreatorProcess(const std::string& process){ _creator_process = process;}
+  inline const std::string& Particle::GetCreatorProcess() const{ return _creator_process;}
+
+  inline void Particle::SetMother(const Particle* mother ){ _mother = mother; }
+  //inline const Particle* Particle::GetMother(const Particle* mother ){ return _mother; }
+  inline void Particle::AddTrack(const Track* track ){ _tracks->AddLast(track); }
+  inline const TRefArray& Particle::GetTracks(){ return _tracks; }
+  inline void Particle::AddDaughter(const Particle* particle ){ _particles->AddLast(particle); }
+  inline const TRefArray& Particle::GetDaughters(){ return _particles; }
+
+} // namespace nuEI
+
+ostream& operator << (ostream& s, const nuEI::Particle& p);
+
+#endif // __nuEI_PARTICLE__

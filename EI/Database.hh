4aca477d8252b611ede127aec7b15575bfd739ba
gonzaponte <gonzaponte@gmail.com>
2015-08-11 13:12:16 +0200

    several bugs

--- EI/Database.hh
+++ EI/Database.hh
@@ -3,18 +3,18 @@
 ////  Database contaning the particles' information.
 ////
 ////  @Author   Gonzalo Martínez Lema
-////  @Date     09/08/2015
-////  @Mofidied 09/08/2015
+////  @Date     10/08/2015
+////  @Mofidied 10/08/2015
 ////
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef __nuEI_DATABASE__
 #define __nuEI_DATABASE__
 
+#include <TObject.h>
+
 #include <string>
 #include <map>
-
-#include <TObject.h>
 
 namespace nuEI
 {
@@ -22,49 +22,52 @@ namespace nuEI
   {
 
   public:
+    static Database* GetInstance();
+
+  private:
     // Empty constructor
      Database();
     // Destructor
-    ~Database();
+    ~Database(){ }
 
-  private:
+    static Database*             _instance;  // instance of the class
     std::map< std::string, int > _codes;     // key: name value: code
     std::map< int, std::string > _names;     // key: code value: name
     std::map< int, double >      _masses;    // key: code value: mass
     std::map< int, int >         _charges;   // key: code value: charge
     std::map< int, double >      _lifetimes; // key: code value: lifetime
 
-    void LoadDatabase( std::string& );
+    void LoadDatabase();
 
   public:
-    int GetPDGcode( std::string& name ) const;
-    std::string GetName( int code ) const ;
+    int GetPDGcode( std::string& name ) ;
+    std::string GetName( int code )  ;
 
-    double GetMass( int code ) const;
-    double GetMass( std::string& name ) const;
+    double GetMass( int code ) ;
+    double GetMass( std::string& name ) ;
 
-    double GetCharge( int code ) const;
-    double GetCharge( std::string& name ) const;
+    double GetCharge( int code ) ;
+    double GetCharge( std::string& name ) ;
 
-    double GetLifetime( int code ) const;
-    double GetLifetime( std::string& name ) const;
+    double GetLifetime( int code ) ;
+    double GetLifetime( std::string& name ) ;
 
     ClassDef(Database,1);
 
   }; // class Database
 
   // INLINE functions
-  inline int GetPDGcode( std::string& name ) const{ return _codes[name];}
-  inline std::string GetName( int code ) const{ return _names[code];}
+  inline int Database::GetPDGcode( std::string& name ) { return _codes[name];}
+  inline std::string Database::GetName( int code ) { return _names[code];}
 
-  inline double GetMass( int code ) const{ return _masses[code];}
-  inline double GetMass( std::string& name ) const{ return GetMass(_codes[name]);}
+  inline double Database::GetMass( int code ) { return _masses[code];}
+  inline double Database::GetMass( std::string& name ) { return GetMass(_codes[name]);}
 
-  inline double GetCharge( int code ) const{ return _charges[code];}
-  inline double GetCharge( std::string& name ) const{ return GetCharge(_codes[name]);}
+  inline double Database::GetCharge( int code ) { return _charges[code];}
+  inline double Database::GetCharge( std::string& name ) { return GetCharge(_codes[name]);}
 
-  inline double GetLifetime( int code ) const{ return _lifetimes[code];}
-  inline double GetLifetime( std::string& name ) const{ return GetLifetime(_codes[name]);}
+  inline double Database::GetLifetime( int code ) { return _lifetimes[code];}
+  inline double Database::GetLifetime( std::string& name ) { return GetLifetime(_codes[name]);}
 
 } // namespace nuEI
 
